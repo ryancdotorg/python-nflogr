4e062be60c6a4bbdf0df777929937475e6faf117
Ryan Castellucci <code@ryanc.org>
2021-02-28 09:01:09 +0000

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 import setuptools
 from distutils.core import setup, Extension
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="nflogr",
-    version='0.0.1',
+    version='0.0.2',
     description='An object-oriented Python interface to read data via NFLOG',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ryan Castellucci',
     author_email='pypi-b51f@ryanc.org',
     license='MIT',
@@ -15,7 +23,7 @@ setup(
         libraries=["netfilter_log", "nfnetlink"]
         )],
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
