6eea1c2887e7879510ec349840f2817f82e4ce4c
Ryan Castellucci <code@ryanc.org>
2021-03-01 16:48:11 +0000

    v0.0.3

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ make_constants('linux/if_ether.h', 'proto', r'.+ETH_P_(\S+)\s+(\S+).*')
 
 setup(
     name="nflogr",
-    version='0.0.2',
+    version='0.0.3',
     description='An object-oriented Python interface to read data via NFLOG',
     long_description=long_description,
     long_description_content_type='text/markdown',
