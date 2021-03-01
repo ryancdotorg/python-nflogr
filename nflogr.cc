417f87867c7784d2a30d08d0969654491198d82e
Ryan Castellucci <code@ryanc.org>
2021-03-01 16:35:52 +0000

    refactor nflogr.cc

--- nflogr.cc
+++ nflogr.cc
@@ -3,9 +3,12 @@
 #include <Python.h>
 
 #include <netinet/in.h>
+#include <errno.h>
 
 extern "C" {
 #include <libnetfilter_log/libnetfilter_log.h>
+#include <libnfnetlink/libnfnetlink.h>
+#include <linux/netlink.h>
 }
 
 #include "nflogr.h"
@@ -16,14 +19,63 @@ extern "C" {
 PyObject *NflogError;
 PyObject *NflogClosedError;
 
-static PyObject * l_open(PyObject *self, PyObject *args) {
-  int group;
+static int _nflog_bind_pf(struct nflog_handle *h, u_int16_t pf) {
+  // XXX Some example code tries to do nflog_unbind_pf first, but the docs say
+  // it is dangerous, prone to breaking other software, and should not be used.
+  if (nflog_bind_pf(h, pf) != 0) {
+    if (errno == EPERM) {
+      PyErr_SetString(
+        PyExc_PermissionError,
+        "could not bind protocol family (are you root?)"
+      );
+    } else {
+      PyErr_Format(
+        PyExc_OSError,
+        "could not bind protocol family: %s (%d)",
+        strerror(errno), errno
+      );
+    }
+    return -1;
+  }
+  return 0;
+}
 
-  if (!PyArg_ParseTuple(args, "i:open", &group)) {
-    PyErr_SetString(PyExc_TypeError, "group must be an integer");
+static PyObject * l_open(PyObject *self, PyObject *args, PyObject *kwargs) {
+  int opt, group;
+
+  int enobufs = 1;
+  long long qthresh = 1, nlbufsiz = 0;
+  unsigned char copymode = NFULNL_COPY_PACKET;
+  double timeout = 0.00;
+
+  static const char *kwlist[] = {
+    "group", "copymode", "timeout", "qthresh", "nlbufsiz", "enobufs",
+    NULL
+  };
+  if (!PyArg_ParseTupleAndKeywords(
+    args, kwargs, "i|$bdLLp:open", (char **)kwlist,
+    &group, &copymode, &timeout, &qthresh, &nlbufsiz, &enobufs
+  )) {
     return NULL;
-  } else if (group < 0 || group > 65535) {
-    PyErr_SetString(PyExc_ValueError, "valid group range is [0,65535]");
+  }
+
+  if (group < 0 || group > 65535) {
+    PyErr_SetString(PyExc_ValueError, "group value must be in range [0,65535]");
+    return NULL;
+  }
+
+  if (timeout < 0 || timeout > 42949672.951) {
+    PyErr_SetString(PyExc_ValueError, "timeout value must be in range [0.00,42949672.95]");
+    return NULL;
+  }
+
+  if (qthresh < 0 || qthresh > 4294967295) {
+    PyErr_SetString(PyExc_ValueError, "qthresh value must be in range [0,4294967295]");
+    return NULL;
+  }
+
+  if (nlbufsiz < 0 || nlbufsiz > 4294967295) {
+    PyErr_SetString(PyExc_ValueError, "nlbufsiz value must be in range [0,4294967295]");
     return NULL;
   }
 
@@ -31,27 +83,67 @@ static PyObject * l_open(PyObject *self, PyObject *args) {
   struct nflog_g_handle *gh;
 
   if (!(h = nflog_open())) {
-    PyErr_SetString(PyExc_PermissionError, "could not get nflog handle");
-    return NULL;
-  }
-  if (nflog_bind_pf(h, AF_INET) < 0) {
-    nflog_close(h);
-    PyErr_SetString(PyExc_PermissionError, "could not bind protocol family (are you root?)");
-    return NULL;
-  }
-  if (!(gh = nflog_bind_group(h, group))) {
-    nflog_close(h);
-    PyErr_SetString(PyExc_OSError, "could not get group handle");
-    return NULL;
-  }
-  if (nflog_set_mode(gh, NFULNL_COPY_PACKET, 0xffff) < 0) {
-    nflog_unbind_group(gh);
-    nflog_close(h);
-    PyErr_SetString(PyExc_PermissionError, "could not set packet copy mode");
+    PyErr_SetString(PyExc_OSError, "could not open nflog handle");
     return NULL;
   }
 
+  if (_nflog_bind_pf(h, PF_INET) != 0) { goto l_open_cleanup_h; }
+  if (_nflog_bind_pf(h, PF_INET6) != 0) { goto l_open_cleanup_h; }
+
+  errno = 0;
+  if (!(gh = nflog_bind_group(h, group))) {
+    if (errno == EPERM) {
+      PyErr_Format(
+        PyExc_PermissionError,
+        "could not bind nflog group %i, it may be in use, see "
+        "/proc/net/netfilter/nfnetlink_log",
+        group
+      );
+    } else {
+      PyErr_Format(
+        PyExc_OSError,
+        "could not bind nflog group %i: %s (%d)",
+        group, strerror(errno), errno
+      );
+    }
+    goto l_open_cleanup_h;
+  }
+
+  if (nflog_set_mode(gh, copymode, 0xffff) != 0) {
+    PyErr_SetString(PyExc_OSError, "could not set packet copy mode");
+    goto l_open_cleanup_gh;
+  }
+
+  if (nflog_set_qthresh(gh, qthresh) != 0) {
+    PyErr_SetString(PyExc_OSError, "could not set qthresh");
+    goto l_open_cleanup_gh;
+  }
+
+  if (nlbufsiz > 0 && nflog_set_nlbufsiz(gh, nlbufsiz) != 0) {
+    PyErr_SetString(PyExc_OSError, "could not set nlbufsiz");
+    goto l_open_cleanup_gh;
+  }
+
+  if (nflog_set_timeout(gh, timeout * 100.0) != 0) {
+    PyErr_SetString(PyExc_OSError, "could not set timeout");
+    goto l_open_cleanup_gh;
+  }
+
+  if (!enobufs) {
+    opt = 1;
+    if (setsockopt(nflog_fd(h), SOL_NETLINK, NETLINK_NO_ENOBUFS, &opt, sizeof(int)) != 0) {
+      PyErr_SetString(PyExc_OSError, "could not set NO_ENOBUFS");
+      goto l_open_cleanup_gh;
+    }
+  }
+
   return new_nflogobject(h, gh, group);
+
+l_open_cleanup_gh:
+  nflog_unbind_group(gh);
+l_open_cleanup_h:
+  nflog_close(h);
+  return NULL;
 }
 
 static PyObject * l__from_iter(PyObject *self, PyObject *args) {
@@ -66,12 +158,13 @@ static PyObject * l__from_iter(PyObject *self, PyObject *args) {
 }
 
 static PyMethodDef nflogrMethods[] = {
-  {"open", l_open, METH_VARARGS, PyDoc_STR(
-    "open(group)\n"
+  {"open", (PyCFunction)l_open, METH_VARARGS | METH_KEYWORDS, PyDoc_STR(
+    "open(group, copymode=nflogr.COPY_PACKET, timeout=0.00, qthresh=1,"
+    "nlbufsiz=0, enobufs=True)\n"
     "--\n\n"
     "Open an nflog listener for the specifed group."
   )},
-  {"_from_iter", l__from_iter, METH_VARARGS, PyDoc_STR(
+  {"_from_iter", (PyCFunction)l__from_iter, METH_VARARGS, PyDoc_STR(
     "_from_iter(iterator)\n"
     "--\n\n"
     "Open a mock nflog 'listener' which pulls messages from an iterator.\n"
@@ -82,21 +175,19 @@ static PyMethodDef nflogrMethods[] = {
 
 static struct PyModuleDef nflogr_module = {
   PyModuleDef_HEAD_INIT,
-  "nflog",
-  "Interface for accessing nflog.",
+  "nflogr",
+  "An object-oriented Python interface to read data via NFLOG",
   0,  // m_size - this module has no state
   nflogrMethods
 };
 
 #define MOD_ADD_OBJ(M, N, O) \
-  do { \
-    Py_INCREF(O); \
-    if (PyModule_AddObject(M, N, (PyObject *)(O)) < 0) { \
-      Py_DECREF(M); \
-      Py_DECREF(O); \
-      return NULL; \
-    } \
-  } while (0);
+  Py_XINCREF(O); \
+  if (PyModule_AddObject(M, N, (PyObject *)(O)) < 0) { \
+    Py_XDECREF(O); \
+    Py_DECREF(M); \
+    return NULL; \
+  }
 
 PyMODINIT_FUNC PyInit_nflogr(void) {
   PyObject *m;
