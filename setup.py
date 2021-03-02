542410ca54e77e62a8a93416b055495373d13619
Ryan Castellucci <code@ryanc.org>
2021-03-02 08:09:54 +0000

    updates to setup.py

--- setup.py
+++ setup.py
@@ -15,42 +15,45 @@ def make_constants(header, prefix, regex):
     hfile = '/usr/include/' + header
     out = path.join(this_directory, 'nflogconst%s.cc' % prefix)
     if path.isfile(hfile):
+        # get checksum of header file
         with open(hfile, 'rb') as f:
             cksum = sha256(f.read()).hexdigest()
 
+        # if checksum matches, don't regenerate
         if path.isfile(out):
             with open(out) as f:
                 for line in f.readlines():
                     if cksum in line:
                         return None
-        r = re.compile(regex)
-        h = open(hfile)
-        output = [
-            warn,
-            '/* %s */' % cksum,
-            '#include <Python.h>',
-            '#include <%s>' % header,
-            '#include "nflogconst.h"',
-            'int nflog_add_%ss(PyObject *m) {' % prefix,
-            '  if (!m) { return -1; }'
-        ]
-        for line in h.readlines():
-            m = r.match(line)
-            if m:
-                tup = (prefix.upper(), m.group(1), m.group(2))
-                output.append('  ADDINTCONST(m, "%s_%s", %s);' % tup)
-        output.append('  return 0;\n}')
-        h.close()
 
-        f = open(out, 'w')
-        f.writelines(map(lambda x: x+'\n', output))
-        f.close()
+        # generate lines of output file
+        r = re.compile(regex)
+        with open(hfile) as h:
+            output = [
+                warn,
+                '/* %s */' % cksum,
+                '#include <Python.h>',
+                '#include <%s>' % header,
+                '#include "nflogconst.h"',
+                'int nflog_add_%ss(PyObject *m) {' % prefix,
+                '  if (!m) { return -1; }'
+            ]
+            for line in h.readlines():
+                m = r.match(line)
+                if m:
+                    tup = (prefix.upper(), m.group(1), m.group(2))
+                    output.append('  ADDINTCONST(m, "%s_%s", %s);' % tup)
+            output.append('  return 0;\n}')
+
+        # actually write output file
+        with open(out, 'w') as f:
+            f.writelines(map(lambda x: x+'\n', output))
     else:
+        # fall back to pregenerated file if header can't be found
         with open(path.join(this_directory, 'nflogconst%s_def.cc' % prefix)) as i:
             with open(out, 'w') as o:
                 o.write(warn+'\n')
-                for line in i.readlines():
-                    o.write(line)
+                o.writelines(i.readlines())
 
 make_constants('linux/if_arp.h', 'hwtype', r'.+ARPHRD_(\S+)\s+(\S+).*')
 make_constants('linux/if_ether.h', 'proto', r'.+ETH_P_(\S+)\s+(\S+).*')
@@ -81,6 +84,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: C',
+        'Programming Language :: C++',
         'Programming Language :: Python :: 3.5',
         'Topic :: Security',
         'Topic :: Communications',
