ce9072fe7feedd20390beb3d8e13a7443ea9cc19
Ryan Castellucci <code@ryanc.org>
2021-03-07 16:53:02 +0000

    v0.2.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md')) as f:
 
 pkg_attrs = dict(
     name='nflogr',
-    version='0.2.7',
+    version='0.2.8',
     description='An object-oriented Python interface to read data via NFLOG',
     long_description=long_description,
     long_description_content_type='text/markdown',
